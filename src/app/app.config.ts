31f573ff0c44078735e3c8b19f4bf3cff47a0f20
MarielaJara <mayi2595@gmail.com>
2024-05-09 21:12:13 -0600

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideHttpClient(withFetch()),
   ],
 };
