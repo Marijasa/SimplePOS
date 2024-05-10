b52942c5f852ccaf92ff2db1aa6b55b07cda8cd4
MarielaJara <mayi2595@gmail.com>
2024-05-09 21:12:05 -0600

    active menu

--- src/app/componentes/main/main.component.ts
+++ src/app/componentes/main/main.component.ts
@@ -8,4 +8,26 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+
+  constructor(private router: Router) {
+    this.checkActive();
+  }
+
+  checkActive() {
+    setTimeout(() => {
+      const url = this.router.url;
+      console.log('url', url);
+      const navLinks = document.querySelectorAll('.nav-link');
+      navLinks.forEach((navLink:Element) => {
+        const href = navLink.getAttribute('href');
+        if (href === url) {
+          navLink.parentElement?.classList.add('active');
+        } else {
+          navLink.parentElement?.classList.remove('active');
+        }
+      });
+    }, 100);
+
+  }
+}
